361d4ca669fd8c14343bfcf8e3385c9b23a87584
Ruslan Gataullin <fisher7410@yandex.ru>
2021-08-24 15:16:07 +0500

    Updated response handling

--- src/Decorators/ResponseStateDTODecorator.php
+++ src/Decorators/ResponseStateDTODecorator.php
@@ -4,41 +4,50 @@ namespace Albion\Status\Decorators;
 
 use Albion\Status\DTOs\ServiceStateDTO;
 use Albion\Status\Models\State;
+use DateTime;
 use GuzzleHttp\Exception\RequestException;
 use Psr\Http\Message\ResponseInterface;
 
 class ResponseStateDTODecorator
 {
-    public function decorate(ResponseInterface $response): ServiceStateDTO {
+    protected function isDTTime(): bool
+    {
+        $dtStartTime = (new DateTime('now'))->setTime(10, 00, 00, 00);
+        $dtEndTime   = (new DateTime('now'))->setTime(11, 00, 00, 00);
+        $now         = new DateTime();
+
+        return $dtStartTime < $now && $now < $dtEndTime;
+    }
+
+    public function decorate(ResponseInterface $response): ServiceStateDTO
+    {
         try {
-            switch ($response->getStatusCode()) {
-                case 200:
-                    $data = trim($response->getBody()->getContents());
+            $data = trim($response->getBody()->getContents());
 
-                    // Clear BOM chars from response
-                    if (0 === strpos(bin2hex($data), 'efbbbf')) {
-                        $data = substr($data, 3);
-                    }
+            // Clear BOM chars from response
+            if (0 === strpos(bin2hex($data), 'efbbbf')) {
+                $data = substr($data, 3);
+            }
 
-                    $content = json_decode($data, true);
+            $content = json_decode($data, true);
 
-                    switch ($content['status']) {
-                        case 'online':
-                            return new ServiceStateDTO(State::of(State::ONLINE), $content['message']);
+            switch ($content['status']) {
+                case 'online':
+                    return new ServiceStateDTO(State::of(State::ONLINE), $content['message']);
 
-                        case 'offline':
-                            return new ServiceStateDTO(State::of(State::OFFLINE), $content['message']);
+                case 'offline':
+                    return new ServiceStateDTO(State::of(State::OFFLINE), $content['message']);
 
-                        case 'starting':
-                            return new ServiceStateDTO(State::of(State::STARTING), $content['message']);
+                case 'starting':
+                    return new ServiceStateDTO(State::of(State::STARTING), $content['message']);
 
-                        default:
-                            return new ServiceStateDTO(State::of(State::FAILED), 'Failed to resolve service state');
-                    }
-
-                case 500:
                 default:
-                    return new ServiceStateDTO(State::of(State::UNRESPONSIVE), $response->getReasonPhrase());
+
+                    if ($this->isDTTime()) {
+                        return new ServiceStateDTO(State::of(State::OFFLINE), $content['message']);
+                    }
+
+                    return new ServiceStateDTO(State::of(State::FAILED), 'Failed to resolve service state');
             }
         } catch (RequestException $exception) {
             return new ServiceStateDTO(State::of(State::UNRESPONSIVE), $exception->getMessage());
